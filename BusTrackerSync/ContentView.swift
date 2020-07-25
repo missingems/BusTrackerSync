ac32b4391d64bce3b80a39d4500f1baf5b85eafb
Jun <missingems@gmail.com>
2020-07-25 14:25:16 +0800

    Initial Commit

---
+++ BusTrackerSync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusTrackerSync
+//
+//  Created by Jun Xiu Chan on 25/7/20.
+//  Copyright © 2020 Jun Xiu Chan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BusTrackerSyncTests/BusTrackerSyncTests.swift
@@ -0,0 +1,34 @@
+//
+//  BusTrackerSyncTests.swift
+//  BusTrackerSyncTests
+//
+//  Created by Jun Xiu Chan on 25/7/20.
+//  Copyright © 2020 Jun Xiu Chan. All rights reserved.
+//
+
+import XCTest
+@testable import BusTrackerSync
+
+class BusTrackerSyncTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
